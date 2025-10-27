af8e7e804d07c99b25437f7f73a600ea8ccdbf76
ashotvdovychenko <129285657+ashotvdovychenko@users.noreply.github.com>
2025-10-27 23:21:41 +0200

    First practice lesson. Hello world workflow.

---
+++ 1-hello-world-workflow/src/main/java/org/example/HelloWorldWorkflow.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import io.temporal.workflow.WorkflowInterface;
+import io.temporal.workflow.WorkflowMethod;
+
+@WorkflowInterface
+public interface HelloWorldWorkflow {
+
+    @WorkflowMethod
+    String sayHelloWorld(String name);
+}
